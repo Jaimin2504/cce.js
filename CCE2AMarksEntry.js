4151fc40df9187a550ca2d246127aa2901a68aee
Jaimin2504 <jaiminpatel2504@gmail.com>
2016-07-27 13:06:44 +0530

    Update CCE2AMarksEntry.js

--- CCE2AMarksEntry.js
+++ CCE2AMarksEntry.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
+$(document).ready(function () {
 
     $("#sidebar").find("#btnMarksEntryCoscholasticSixtoEight").on('click', function (e) {
         e.preventDefault();
@@ -10,6 +10,27 @@
                 $("#top").find(".containerDiv").show();
             },
             url: '/CCE2AMarksEntry/TwoAMarksEntryDetailSixtoEight',
+            type: this.method,
+            data: {},
+            success: function (result) {
+                $("#container").find("#main").html(result);
+                $(window).scrollTop(0);
+                $("#top").find(".containerDiv").hide();
+            }
+
+        });
+    });
+
+    $("#sidebar").find("#btnIndicatorMarksEntryCoscholasticSixtoEight").on('click', function (e) {
+        e.preventDefault();
+        //e.stopPropagation();
+
+        $.ajax({
+
+            beforeSend: function () {
+                $("#top").find(".containerDiv").show();
+            },
+            url: '/CCE2AMarksEntry/IndicatorMarksEntryCoscholasticSixtoEight',
             type: this.method,
             data: {},
             success: function (result) {
@@ -53,8 +74,8 @@ $(function () {
                 var AvgMarks = $(this).closest('tr').find('.AvgMarks').find('input').val();
                 if (ActivityId > 0) {
                     //var node = '<MarksEntry StudentId="' + StudentId + '" ActivityId ="' + ActivityId  + '" Marks="' + Marks + '" />';
-                    var node = '<node><StudentId>' + StudentId + '</StudentId><ActivityId >' + ActivityId + '</ActivityId ><Marks>' + Marks + '</Marks><AvgMarks>' + AvgMarks + '</AvgMarks></node>';
-                    node = node.replace("<node><StudentId>undefined</StudentId><ActivityId >undefined</ActivityId ><Marks>undefined</Marks><AvgMarks>undefined</AvgMarks></node>", ' ');
+                    var node = '<node><StudentId>' + StudentId + '</StudentId><ActivityId>' + ActivityId + '</ActivityId><Marks>' + Marks + '</Marks><AvgMarks>' + AvgMarks + '</AvgMarks></node>';
+                    node = node.replace("<node><StudentId>undefined</StudentId><ActivityId>undefined</ActivityId><Marks>undefined</Marks><AvgMarks>undefined</AvgMarks></node>", ' ');
                     info += node;
                 }
 
@@ -197,3 +218,136 @@ $('#FormMarksEntryDetailSixtoEight').find('#tblMarksEntryDetailSixtoEight').find
     $(this).closest('tr').find('.AvgMarks').find('input').val(AverageMarks);
 
 });
+
+
+$(function () {
+    var btnsubmit;
+    $("#FormIndicatorMarksEntryDetailSixtoEight").find('input[type="submit"]').live('click', function () {
+        btnsubmit = $(this).val();
+    })
+
+    $('#FormIndicatorMarksEntryDetailSixtoEight').live('submit', function (e) {
+        e.preventDefault();
+        //e.stopPropagation();
+
+        var Courselevel = $(this).find('#ddlCourselevel').val();
+        var Course = $(this).find('#ddlCourse').val();
+        var Division = $(this).find('#ddlDivision').val();
+        var Type = $(this).find('#ddlActivity').val();
+        var Group = $(this).find('#ddlGroup').val();
+        //var btnsubmit = $(this).find('input[type="submit"]').val();
+        var msg = "";
+
+
+        var info = "<root>";
+
+
+        $('#FormIndicatorMarksEntryDetailSixtoEight').find('#tblIndicatorMarksEntryDetailSixtoEight').find('tbody tr').each(function () {
+            var StudentId = $(this).attr("id");
+            var IndicatorId = $(this).find('.Indicator').find('.ddlIndicator').val();
+            var node = '<node><StudentId>' + StudentId + '</StudentId><IndicatorId>' + IndicatorId + '</IndicatorId></node>';
+            node = node.replace("<node><StudentId>undefined</StudentId><IndicatorId>undefined</IndicatorId></node>", ' ');
+            info += node;
+            //$(this).find('.ActivityMarks').each(function () {
+            //    var ActivityId = $(this).find('input').attr('id');
+            //    var Marks = $(this).find('input').val();
+            //    var AvgMarks = $(this).closest('tr').find('.AvgMarks').find('input').val();
+            //    if (ActivityId > 0) {
+            //        //var node = '<MarksEntry StudentId="' + StudentId + '" ActivityId ="' + ActivityId  + '" Marks="' + Marks + '" />';
+            //        var node = '<node><StudentId>' + StudentId + '</StudentId><ActivityId >' + ActivityId + '</ActivityId ><Marks>' + Marks + '</Marks><AvgMarks>' + AvgMarks + '</AvgMarks></node>';
+            //        node = node.replace("<node><StudentId>undefined</StudentId><ActivityId >undefined</ActivityId ><Marks>undefined</Marks><AvgMarks>undefined</AvgMarks></node>", ' ');
+            //        info += node;
+            //    }
+
+
+
+            //});
+        });
+        info += "</root>";
+        $('#FormIndicatorMarksEntryDetailSixtoEight').find('#MarksXML').val(info);
+        var FD = $(this).serialize();
+
+        if (Courselevel == '' || Courselevel == 0 || Courselevel == undefined) {
+            $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlCourselevel').css("border", "1px solid red");
+            setTimeout(function () {
+                $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlCourselevel').css("border", "");
+            }, 10000);
+            msg += "Medium is required.,\n";
+        }
+        if (Course == '' || Course == 0 || Course == undefined) {
+            $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlCourse').css("border", "1px solid red");
+            setTimeout(function () {
+                $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlCourse').css("border", "");
+            }, 10000);
+            msg += "Standard is required.,\n";
+        }
+        if (Division == '' || Division == 0 || Division == undefined) {
+            $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlDivision').css("border", "1px solid red");
+            setTimeout(function () {
+                $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlDivision').css("border", "");
+            }, 10000);
+            msg += "Division is required.,\n";
+        }
+        if (Type == '' || Type == 0 || Type == undefined) {
+            $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlActivity').css("border", "1px solid red");
+            setTimeout(function () {
+                $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlActivity').css("border", "");
+            }, 10000);
+            msg += "Activity Name is required.,\n";
+        }
+        if (Group == '' || Group == 0 || Group == undefined) {
+            $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlGroup').css("border", "1px solid red");
+            setTimeout(function () {
+                $("#FormIndicatorMarksEntryDetailSixtoEight").find('#ddlGroup').css("border", "");
+            }, 10000);
+            msg += "Group Name is required.,\n";
+        }
+
+        if (msg != '') {
+            alert(msg);
+            return false;
+        }
+
+        if (btnsubmit == "Search") {
+            $.ajax({
+
+                beforeSend: function () {
+                    $("#top").find(".containerDiv").show();
+                },
+                url: '/CCE2AMarksEntry/IndicatorMarksEntryCoscholasticSixtoEight',
+                type: "GET",
+                data: { Courselevelid: Courselevel, Courseid: Course, Divisionid: Division, Typeid: Type, Groupid: Group },
+                success: function (result) {
+                    $("#container").find("#main").html(result);
+                    $(window).scrollTop(0);
+                    $("#top").find(".containerDiv").hide();
+                }
+
+            });
+        }
+
+        if (btnsubmit == "Save") {
+            
+            $.ajax({
+
+                beforeSend: function () {
+                    $("#top").find(".containerDiv").show();
+                },
+                //url: '/CCEPrimaryThirdtoEight/AssignMarksThirdtoEight?MARKXML=' + info,
+                url: '/CCE2AMarksEntry/IndicatorMarksEntryCoscholasticSixtoEight',
+                type: "POST",
+                data: FD,
+                success: function (result) {
+                    if (result != '') {
+                        alert('Inserted Successfully !!!');
+                    }
+                    $("#top").find(".containerDiv").hide();
+                    $("#sidebar").find("#btnIndicatorMarksEntryCoscholasticSixtoEight").click();
+                }
+
+            });
+        }
+    });
+
+});
+
